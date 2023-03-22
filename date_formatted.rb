8add67ab0dbee5f767ac303182acb417e6df31e4
William Price <price.james.william@gmail.com>
2023-03-22 19:23:31 +0000

    date

--- date_formatted.rb
+++ date_formatted.rb
@@ -1,3 +1,13 @@
 # Should format and identify different parts of todays date.
-# 
+#
 # "The year is: 2022, the calendar day is: 1, and the month is: 7."
+
+require "date"
+
+today = Date.today
+
+year = today.year
+day = today.day
+month = today.month
+
+puts "The year is: #{year}, the calendar day is: #{day}, and the month is: #{month}."
--- date_monday.rb
+++ date_monday.rb
@@ -1,10 +1,17 @@
 # Should output:
-# 
+#
 #   "Is today Monday? true"
-# 
+#
 # if today is Monday, or
-# 
+#
 #   "Is today Monday? false"
-# 
+#
 # if today is not Monday.
 
+require "date"
+
+today = Date.today
+
+is_monday = today.monday?
+
+puts "Is today Monday? #{is_monday}?"
